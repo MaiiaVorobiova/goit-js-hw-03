337528c5363b69844099b6e8a1ff852eb00f73a3
MaiiaVorobiova <mayaevege@gmail.com>
2020-12-20 20:39:59 +0200

    добавены скрипты

---
+++ task1.js
@@ -0,0 +1,18 @@
+'use strict'
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+user['mood'] = 'happy';
+console.log(JSON.stringify(user));
+user['hobby'] = 'skydiving';
+console.table(user);
+user['premium'] = false;
+console.table(user);
+const userKeys = Object.keys(user);
+
+for (const key of userKeys) {
+    console.log(`${key}: ${user[key]}`);
+}
